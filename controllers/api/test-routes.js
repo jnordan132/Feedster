768f29364a07183a7c1d18f447f97e4c859f5717
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-09-15 12:48:18 -0400

    created a delete by id route for test routes

--- controllers/api/test-routes.js
+++ controllers/api/test-routes.js
@@ -1,18 +1,33 @@
 const router = require("express").Router();
 const { Test } = require("../../models");
 
-router.get("/", async (req, res) => {
+router.get("/", async(req, res) => {
     console.log("get test from api (node)");
     return res.status(200).json("test!");
 });
 
 //create comment
-router.post("/", async (req, res) => {
+router.post("/", async(req, res) => {
     try {
         const testData = await Test.create({
             test: req.body.test,
         });
         return res.status(200).json(testData);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+
+// delete comment
+router.delete("/:id", async(req, res) => {
+    try {
+        const deleteCommentData = await Test.destroy({
+            where: {
+                test: req.body.test,
+            }
+        });
+        return res.status(200).json(deleteCommentData);
     } catch (err) {
         console.log(err);
         return res.status(500).json(err);
