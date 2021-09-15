602c75241f4107490ba76188b9a20c4baa2917c4
David Tunnell <davidtunnell9@gmail.com>
2021-09-15 15:49:58 -0500

    user api for signup, login, logout now working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const testRoutes = require("./test-routes.js");
+const userRoutes = require("./users-routes.js");
 
 //pass each API route to router
 router.use("/test", testRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
