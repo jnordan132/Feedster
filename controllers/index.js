981db097bde8fe16a865d0554a77614eb91e8b78
David Tunnell <davidtunnell9@gmail.com>
2021-09-16 16:12:47 -0500

    login/logout/signup working on client side with api

--- controllers/index.js
+++ controllers/index.js
@@ -7,10 +7,31 @@ const apiRoutes = require("./api");
 router.use("/test", testRoutes);
 router.use("/api", apiRoutes);
 
+router.get("/", async (req, res) => {
+    res.render("index", {
+        loggedIn: req.session.loggedIn,
+        loggedInUserData: req.session.loggedInUserData,
+    });
+});
+
 router.get("/signup", async (req, res) => {
     res.render("signup", {
         loggedIn: req.session.loggedIn,
         loggedInUserData: req.session.loggedInUserData,
+    });
+});
+
+router.get("/login", async (req, res) => {
+    res.render("login", {
+        loggedIn: req.session.loggedIn,
+        loggedInUserData: req.session.loggedInUserData,
+    });
+});
+
+router.get("/logout", async (req, res) => {
+    res.render("logout", {
+        loggedIn: false,
+        loggedInUserData: null,
     });
 });
 
---
+++ public/js/logout.js
@@ -0,0 +1,13 @@
+//logout user from client to api
+const logout = async () => {
+    const response = await fetch("/api/users/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+        alert("Failed to log out.");
+    }
+};
+
+logout();
