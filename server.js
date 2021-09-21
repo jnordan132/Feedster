5711109b4b70be421f0a594bcc47cd939d89a21a
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-21 00:37:19 -0400

    started work on favicon rendering using middleware and serve-favicon npm module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require("express");
+const favicon = require('serve-favicon');
 const expressHandlebars = require("express-handlebars");
 const session = require("express-session");
 const path = require("path");
@@ -41,6 +42,11 @@ app.use(express.urlencoded({ extended: false }));
 //set public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//favicon as defined: https://www.npmjs.com/package/serve-favicon
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.get('/favicon', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/favicon.ico'))
+);
 
 // GET Route for terms of services page
 app.get('/tos', (req, res) =>
