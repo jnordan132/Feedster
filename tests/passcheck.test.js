9aca4aba42f81dbf2b50f61eec29ec25bb278de9
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-22 15:56:49 -0400

    Created a jest testing framework for the signup page's password fields

--- tests/passcheck.test.js
+++ tests/passcheck.test.js
@@ -1,7 +1,12 @@
+const { not } = require('expect');
 const passLen = require('../public/js/passcheck');
 
 // checks the length of the password is greater than 6
 test("Password character length checked", () => {
+// https://jestjs.io/docs/expect
   expect(passLen("test123!")).toBe(8);
-  expect(passLen("test")).toBe(4);
+  expect(passLen("test")).not.toBe(8);
+  expect(passLen("Password1!")).toBeGreaterThanOrEqual(6);
+  expect(passLen("Password2!")).not.toBeNull();
+  expect(passLen("password3!")).not.toBeNaN();
 });
