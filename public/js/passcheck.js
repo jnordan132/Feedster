bd650f8b9c7345a20d63a7f9d6623db6053b26a4
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-22 15:39:13 -0400

    Created a jest testing framework for the signup page's password fields

--- public/js/passcheck.js
+++ public/js/passcheck.js
@@ -1,55 +1,12 @@
-
-// checking password meets conditions and if confirm password  & password match
-function check(password) {
-    const password = document.getElementById('#typePasswordX');
-    const confirmPassword = document.querySelector("#typePasswordAgainX");
-    const up = document.getElementById('#up');
-    const num = document.getElementById('#num');
-    
-    //match is a function which matches regular expressions
-    // num req
-    if (password.value.match(/[0-9])/)) {
-        num.style.color = 'green';
-    } else {
-        num.style.color = 'red';
-    }
-    // uppercase req
-    if (password.value.match(/[A-Z])/)) {
-        up.style.color = 'green';
-    } else {
-        up.style.color = 'red';
-    }
-
-}
-
-// should the 2 match hide the criteria
-function match(password,confirmPassword) {
-const password = document.getElementById('#typePasswordX');
-const confirmPassword = document.querySelector("#typePasswordAgainX");
-const up = document.getElementById('#up');
-const num = document.getElementById('#num');
-
-    if (password === confirmPassword) {
-        num.style.display = 'none';
-        up.style.display = 'none';
-    } else {
-        num.style.display = 'block';
-        up.style.display = 'block';
-    }
-}
-
-
+//a simple check for the password string length
 function passLen(str) {
-if (str.length>6){
-    console.log (`Password Length is ${str.length} - i.e. greater than 6 characters!`)
-    return str.length;
+    if (str.length > 6) {
+        console.log(`Password Length is ${str.length} - i.e. greater than 6 characters!`)
+        return str.length;
+    } else {
+        console.log(`Password length is not >6 characters, it is: ${str.length} characters in length`);
+        return str.length;
+    }
 }
-else {
-    console.log (`Password length is not >6 characters, it is: ${str.length} characters in length`);
-    return str.length;
-}
-  }
 
 module.exports = passLen;
-module.exports = check;
-module.exports = match;
--- tests/passcheck.test.js
+++ tests/passcheck.test.js
@@ -2,7 +2,6 @@ const passLen = require('../public/js/passcheck');
 
 // checks the length of the password is greater than 6
 test("Password character length checked", () => {
-  
-    expect(passLen("test123!")).toBe(8);
+  expect(passLen("test123!")).toBe(8);
   expect(passLen("test")).toBe(4);
 });
