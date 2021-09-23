6cad22a8145595b3b39017f9b051f9e345258c35
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-23 15:15:02 -0400

    form validation being build out for testing

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,12 @@
-const username = document.querySelector("#typeUsernameX").value.trim();
-const email = document.querySelector("#typeEmailX").value.trim();
-const password = document.querySelector("#typePasswordX").value.trim();
-const confirmPassword = document.querySelector("#typePasswordAgainX").value.trim();
-const agree =document.querySelector("#Terms").value;
-const form = document.getElementById('.form-white');
+
 
  const signupFormHandler = async (event) => {
         event.preventDefault();
 
-        // const username = document.querySelector("#typeUsernameX").value.trim();
-        // const email = document.querySelector("#typeEmailX").value.trim();
-        // const password = document.querySelector("#typePasswordX").value.trim();
-        // const confirmPassword = document.querySelector("#typePasswordAgainX").value.trim();
+        const username = document.querySelector("#typeUsernameX").value.trim();
+        const email = document.querySelector("#typeEmailX").value.trim();
+        const password = document.querySelector("#typePasswordX").value.trim();
+        const confirmPassword = document.querySelector("#typePasswordAgainX").value.trim();
 
         console.log(username);
         //new accounts by default are not admins
@@ -50,77 +45,5 @@ const form = document.getElementById('.form-white');
             alert("Please fill out all fields.");
         }
 
-        FormValidation.formValidation(form, {
-            fields: {
-                username: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The username is required'
-                        },
-                        stringLength: {
-                            min: 3,
-                            message: 'The username must be more than 3 characters long'
-                        },
-                        regexp: {
-                            regexp: /^[a-zA-Z0-9]+$/,
-                            message: 'The username can only consist of alphabets and or numbers'
-                        }
-                    }
-                },
-                email: {
-                    validators: {
-                        notEmpty: {
-                            message: 'An email address is required'
-                        },
-                        emailAddress: {
-                            message: 'The input is not a valid email address'
-                        }
-                    }
-                },
-                password: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The password is required'
-                        },
-                        stringLength: {
-                            min: 6,
-                            message: 'The password must have at least 6 characters'
-                        },
-                        different: {
-                            message: 'The password cannot be the same as username',
-                            compare: function() {
-                                return form.querySelector('[name="username"]').value;
-                            }
-                        }
-                    }
-                },
-                confirmPassword: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The password is required'
-                        },
-                        stringLength: {
-                            min: 6,
-                            message: 'The password must have at least 6 characters'
-                        },
-                        different: {
-                            message: 'The password cannot be the same as username',
-                            compare: function() {
-                                return form.querySelector('[name="password"]').value;
-                            }
-                        }
-                    }
-                },
-                agree: {
-                    validators: {
-                        notEmpty: {
-                            message: 'You must agree with the terms and conditions'
-                        }
-                    }
-                }
-            }
-        });  
-
-    };
-
+    }
     document.querySelector(".submit-signup").addEventListener("click", signupFormHandler);
