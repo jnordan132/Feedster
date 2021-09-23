6adfdab4bd8f083ee480e8ecd769442b77550ae4
David Tunnell <davidtunnell9@gmail.com>
2021-09-23 17:34:40 -0500

    fix for post event assignment

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,37 +1,43 @@
 const submit = document.getElementById("postBtn");
 
 // Event listener for submit comment button
-submit.addEventListener("click", async (event) => {
-    event.preventDefault();
+if (submit) {
+    submit.addEventListener("click", async (event) => {
+        event.preventDefault();
 
-    const commentInput = document.querySelector(".comment-input").value.trim();
-    const loggedInUserId = event.target.getAttribute("data-logged-in-user-id");
-    const currentFeedId = event.target.getAttribute("data-current-feed-id");
-    //post to our api
-    if (commentInput && loggedInUserId && currentFeedId) {
-        const response = await fetch("/api/comments/", {
-            method: "POST",
-            body: JSON.stringify({
-                comment: commentInput,
-                user_id: loggedInUserId,
-                feed_id: currentFeedId,
-            }),
-            headers: { "Content-Type": "application/json" },
-        });
-        if (response.ok) {
-            document.location.replace("/feed/" + currentFeedId);
+        const commentInput = document
+            .querySelector(".comment-input")
+            .value.trim();
+        const loggedInUserId = event.target.getAttribute(
+            "data-logged-in-user-id"
+        );
+        const currentFeedId = event.target.getAttribute("data-current-feed-id");
+        //post to our api
+        if (commentInput && loggedInUserId && currentFeedId) {
+            const response = await fetch("/api/comments/", {
+                method: "POST",
+                body: JSON.stringify({
+                    comment: commentInput,
+                    user_id: loggedInUserId,
+                    feed_id: currentFeedId,
+                }),
+                headers: { "Content-Type": "application/json" },
+            });
+            if (response.ok) {
+                document.location.replace("/feed/" + currentFeedId);
+            } else {
+                alert(
+                    "Failed to follow feed." +
+                        response.status +
+                        ": " +
+                        response.statusText
+                );
+            }
         } else {
-            alert(
-                "Failed to follow feed." +
-                    response.status +
-                    ": " +
-                    response.statusText
-            );
+            alert("Please enter a comment before submitting.");
         }
-    } else {
-        alert("Please enter a comment before submitting.");
-    }
-});
+    });
+}
 
 //decode tweet contents and add links to urls and hashtags
 const tweetTexts = document.querySelectorAll(".timeline-Tweet-text");
